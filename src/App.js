c6ea69f331c1f3008f83a37fca2fe197585fd192
nayabdura <119076137+nayabdura@users.noreply.github.com>
2022-12-09 23:09:44 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import Textform from './components/Textform';
+import About from './components/About';
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+
+
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743';
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className=" App ">
+        <Navbar title="Text Manipulator" Textform="Textform" aboutweb="About" mode={mode} toggleMode={toggleMode} />
+
+        <div className="container">
+
+        </div>
+        <Routes>
+          <Route exact path='/' element={<Textform heading="Enter Your Text Below!" mode={mode}/>}></Route>
+         <Route path="/about" element={<About mode={mode}/>}></Route>
+        </Routes>
+
+      </div>
+
+    </>
   );
 }
 
